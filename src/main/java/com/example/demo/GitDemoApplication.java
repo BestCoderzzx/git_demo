29e7bca060ddbb2c08547bfd833af3c21dba1ad1
zzx <zzx2576@qq.com>
2019-06-23 23:45:32 +0800

    test

--- src/main/java/com/example/demo/GitDemoApplication.java
+++ src/main/java/com/example/demo/GitDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("123");
+		System.out.println("123456");
 		SpringApplication.run(GitDemoApplication.class, args);
 	}
 
